d61403e33ecf175ddcce39aeb17cc581ba5bf158
OricoLeco <oricoleco@gmail.com>
2015-09-21 11:09:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 
-<!DOCTYPE html>
+<<!DOCTYPE html>
 <HTML>
     <HEAD>
         <LINK rel="stylesheet" href="css/style.css">
@@ -11,41 +11,37 @@
     <div id="wrapper">
 
 <?php
-
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
-
-
 include 'XmlToJson.php';
-
 $json= XmlToJson::Parse("https://api.flickr.com/services/feeds/photos_public.gne?tags=chamonix,ski,snow");
 $pictures=json_decode($json,true);
-
-
-
 foreach ($pictures['entry'] as $singlePic)
 {
-
         $author=$singlePic['author']['name'];
-        $authorPage=$singlePic['link'][0]['@attributes']['href'];
-         $title=$singlePic['title'];
+       
+        $title=$singlePic['title'];
        
          $link=$singlePic['link'][1]['@attributes']['href'];
         $imagelink=$singlePic['link'];
+        
+        //this link goes to author conection, we need author's picture
+         $peoplePage=$singlePic['author']['uri']; 
+        
+       
+         $authorPage=str_replace('people','photos',$peoplePage);
+//        print $authorPage;
          
-
  ?>   
 
 <ul id="grid">
     <li><a href="<?= $authorPage?>"><img src="<?= $link?>" ></a></li>
 </ul>      
 <?php   
-  }       
+  } 
 
 ?>     
        
@@ -55,6 +51,3 @@ foreach ($pictures['entry'] as $singlePic)
     </BODY>
      
 </HTML>
-
-
-
